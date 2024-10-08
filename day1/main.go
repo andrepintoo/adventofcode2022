546b4690335cc7d26243307ac652001b2b5e9144
andrepintoo <andresantospinto26@gmail.com>
2024-10-09 01:16:17 +0200

    finished day1

--- day1/main.go
+++ day1/main.go
@@ -1,22 +1,67 @@
 package main
 
-import ("fmt", "log", "os", "bufio")
+import (
+	 "fmt"
+	 "log"
+	 "os"
+	 "bufio"
+	 "strconv"
+)
 
 func main(){
-	fmt.Println("Day 1 part 1")
+	fmt.Println("Day 1")
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	
+	max,max2,max3 := 0,0,0
+	cur := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan(){
 		line := scanner.Text()
-		fmt.Println(line)
+		
+		if len(line)==0{
+			if cur > max {
+				max3 = max2
+				max2 = max
+				max = cur
+			} else if cur > max2 {
+				max3 = max2
+				max2 = cur
+			} else if cur > max3 { 
+				max3 = cur
+			}
+			cur = 0	// reset to the next elf	
+			continue
+		}
+		
+		aux, err := strconv.Atoi(line)
+		if err != nil{
+			log.Fatal(err)
+		}
+		cur += aux
 	}
+	
+	if cur > 0 {
+		if cur > max {
+			max3 = max2
+			max2 = max
+			max = cur
+		} else if cur > max2 {
+			max3 = max2
+			max2 = cur
+		} else if max3 > cur { 
+			max3 = cur
+		}
+	}
+			
+	fmt.Printf("Max calories is %d\n", max)
+	fmt.Printf("Total of top 3 elves is %d\n", max+max2+max3)
 
-	if err := scanner.Err(); err != nil {
+	if err = scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
